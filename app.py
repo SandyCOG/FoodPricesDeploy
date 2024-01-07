4932e75196e38a53ea9d0e353a10e39d4971f4fd
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-01-07 23:27:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-import streamlit as st #imports streamlit library
+import streamlit as st
 
 # Set Page Configuration for a centered Layout
 st.set_page_config(layout="centered")
 
-#configure page background colour and font
+# Configure page background color and font
 st.markdown("""
 <style> 
 body {
-            background-color: #007EA7;
-            font-family: Arial, sans-serif;
-""")
+    background-color: #007EA7;
+    font-family: Arial, sans-serif;
+}
+</style>
+""", unsafe_allow_html=True)
 
 # Sidebar for menus
 with st.sidebar:
-            st.title("Menu")
+    st.title("Menu")
+
