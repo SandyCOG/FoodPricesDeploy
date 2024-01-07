0f6a37fdffd88e52d2ec3838dc05e8676a034b33
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-01-07 23:17:35 +0100

    Create app.py
    First page v1

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st #imports streamlit library
+
+# Set Page Configuration for a centered Layout
+st.set_page_config(layout="centered")
+
+#configure page background colour and font
+st.markdown("""
+<style> 
+body {
+            background-color: #007EA7;
+            font-family: Arial, sans-serif;
+
+# Sidebar for menus
+with st.sidebar:
+st.title("Menu")
